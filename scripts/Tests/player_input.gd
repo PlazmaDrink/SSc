f2ff6c0066574bf7a57593ca758aa43a3565a8a6
Roman Marchenko <roma.marchenko@gmail.com>
2026-02-13 12:37:21 +0100

    Reset

---
+++ scripts/Tests/player_input.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+signal fire_input
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_multiplayer_authority():
+		if Input.is_action_just_pressed("Fire"):
+			fire_input.emit()
---
+++ scripts/Tests/weapon.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var projectileScene: PackedScene
+
+@onready var player_input: Node3D = $"../PlayerInput"
+@onready var projectile_spawn_path: Node3D = $ProjectileSpawnPath
+@onready var marker_3d: Marker3D = $Marker3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player_input.fire_input.connect(_on_fire_input)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_fire_input() -> void:
+	var projectile = projectileScene.instantiate() as RigidBody3D
+	projectile.transform = marker_3d.transform
+	projectile_spawn_path.add_child(projectile, true)
+	
