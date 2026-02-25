dd74d3cc25d520730d6680e594322fe8619fbe91
Roman Marchenko <roma.marchenko@gmail.com>
2026-02-25 12:32:17 +0100

    NewTimeComponent
    Removed Timer from time component. Now its checking current in-game time with GlobalTime class. So time component now reacts to in-game time change

--- scripts/Tests/time_component.gd
+++ scripts/Tests/time_component.gd
@@ -3,30 +3,29 @@ extends Node
 var CurrentTime:float = 0
 var isBusy:bool = false
 
-@onready var timer: Timer = $Timer
-
-signal ActionStart(timerValue: int)
 signal ActionFinished
 
-var action_Start_Time: int = 0
+var action_Finish_Time: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalTime.update_current_time.connect(on_update_current_time)
-	set_timer(5)
-
+	set_timer(10)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if isBusy:
+		CheckIsActionComplete()
 	
 func set_timer(wait_time:int) ->void:
-	timer.wait_time = wait_time
-	timer.start()
-	action_Start_Time = GlobalTime.current_day
-
-func _on_timer_timeout() -> void:
-	print("Timer finished")
-	ActionFinished.emit()
+	action_Finish_Time = GlobalTime.current_day + wait_time
+	isBusy = true
 
 func on_update_current_time(current_time: int)-> void:
-	print(current_time)
+	if isBusy:
+		CheckIsActionComplete()
+
+func CheckIsActionComplete() -> void:
+	if GlobalTime.current_day >= action_Finish_Time:
+		isBusy = false
+		action_Finish_Time = -1
+		ActionFinished.emit()
