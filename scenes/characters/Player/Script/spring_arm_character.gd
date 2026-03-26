1d62de2e52a56442700c89229e64c8d4f18cf39b
Roman Marchenko <roma.marchenko@gmail.com>
2026-03-26 13:43:00 +0100

    Clearing Completed

--- scenes/characters/Player/Script/spring_arm_character.gd
+++ scenes/characters/Player/Script/spring_arm_character.gd
@@ -1,7 +1,12 @@
-extends SpringArmCharacter
+extends Node3D
 
 @onready var player: Character = $".."
 @onready var camera_3d: Camera3D = $SpringArm3D/Camera3D
+
+@export_category("Objects")
+@export var _spring_arm: SpringArm3D = null
+
+const MOUSE_SENSIBILITY: float = 0.005
 
 #bob variables
 const BOB_FREQUENCY = 2.0
@@ -13,13 +18,13 @@ func _ready() -> void:
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * MOUSE_SENSIBILITY)
-		spring_arm_3d.rotate_x(-event.relative.y * MOUSE_SENSIBILITY)
-		spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		_spring_arm.rotate_x(-event.relative.y * MOUSE_SENSIBILITY)
+		_spring_arm.rotation.x = clamp(_spring_arm.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _on_head_bob(time)->void:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQUENCY) * BOB_AMPLITUDE
 	pos.x = sin(time * BOB_FREQUENCY/2) * BOB_AMPLITUDE
-	pos.z = spring_arm_3d.position.z
-	spring_arm_3d.transform.origin = pos
+	pos.z = _spring_arm.position.z
+	_spring_arm.transform.origin = pos
 	print(pos)
