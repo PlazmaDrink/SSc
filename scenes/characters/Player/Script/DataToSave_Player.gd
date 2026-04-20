25c1d0605df982180b6d6bca5a3d9a95deb74dcd
Roman Marchenko <roma.marchenko@gmail.com>
2026-04-20 16:15:43 +0200

    Starting Player Saving

---
+++ scenes/characters/Player/Script/DataToSave_Player.gd
@@ -0,0 +1,10 @@
+class_name DataToSave_Player
+extends DataToSave
+
+func save_properties()->void:
+	super.save_properties()
+func load_properties()->void:
+	super.load_properties()
+
+func set_ref_nodes(inCommonNode, inCustomNode = null)->void:
+	super.set_ref_nodes(inCommonNode)
