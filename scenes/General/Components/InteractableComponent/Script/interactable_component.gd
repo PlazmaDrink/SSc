b13ad2019ef7aac177a8864591f38e55f9782250
Roman Marchenko <roma.marchenko@gmail.com>
2026-05-19 16:56:14 +0200

    Outline Shader shader doesn't work with pickups

--- scenes/General/Components/InteractableComponent/Script/interactable_component.gd
+++ scenes/General/Components/InteractableComponent/Script/interactable_component.gd
@@ -4,32 +4,37 @@ extends Node
 ##Assign MeshInstance3D of parent, so component can control outline appearence
 @export_category("Outline")
 @export var meshToOutline: MeshInstance3D
-@export var outlineMaterial: ShaderMaterial
-@export var OutlineColor: Color = Color.TRANSPARENT
-@export var OutlineWidth: float = 0.0
+@export var OutlineColor: Color = Color.BLACK
+@export var OutlineWidth: float = 1
 @export var Action_to_do: Interaction
+
+const OUTLINE_SHADER_MATERIAL = preload("uid://d1hbc5mvwhs5")
 
 var local_material_instance: ShaderMaterial
 var is_focused:= false
 
 func _ready() -> void:
-	local_material_instance = outlineMaterial.duplicate()
-	local_material_instance.set_shader_parameter("outline_colour", OutlineColor)
-	local_material_instance.set_shader_parameter("outline_width", OutlineWidth)
-
+	_prepare_local_material()
+	
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Interact"):
 		try_interact()
 
+func _prepare_local_material():
+	local_material_instance = OUTLINE_SHADER_MATERIAL.duplicate()
+	local_material_instance.set_shader_parameter("outline_colour", OutlineColor)
+	local_material_instance.set_shader_parameter("outline_width", OutlineWidth)
+
 ## Called when the node enters the scene tree for the first time.
 func getOutlineMaterial()->ShaderMaterial:
-	return outlineMaterial
+	return local_material_instance
 
 func raytrace_enter()->void:
 	if meshToOutline.get_surface_override_material(0) == null:
 		meshToOutline.set_surface_override_material(0, StandardMaterial3D.new())
 	meshToOutline.get_surface_override_material(0).next_pass = local_material_instance
 	is_focused = true
+	print_debug(meshToOutline.get_surface_override_material(0).next_pass)
 	
 func raytrace_exit()->void:
 	meshToOutline.get_surface_override_material(0).set_next_pass(null)
