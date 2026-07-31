53203aa4e2a5a730ec675e6c5cc94c681b4c572d
Roman Marchenko <roma.marchenko@gmail.com>
2026-07-31 16:45:16 +0200

    x

--- scenes/Entities/characters/Player/Script/player_ray_cast_3d.gd
+++ scenes/Entities/characters/Player/Script/player_ray_cast_3d.gd
@@ -10,21 +10,30 @@ var target_component_container
 
 func _input(_event) -> void:
 	if is_colliding():
-		#Condition makes sure function call send only once
 		var collider = get_collider()
-		if collider == null:
-			return
+		if collider == null: return
+		
 		if collider.get_parent().is_in_group("iInteractable") && !rayCastCollisionState:
-			rayCastCollisionState = true
 			target_collider = collider
-			var path:String = target_collider.get_parent().get_path()
-			target_component_container = get_node(path + COMPONENT_CONTAINER_TREE_NAME)
-			target_component_container.get_component(GameEnums.Components.InteractableComponent).raytrace_enter()
+			#Var to track so function call send only once
+			rayCastCollisionState = true
+			target_component_container = find_target_component_container(collider.get_parent())
+			if target_component_container:
+				target_component_container.get_component(GameEnums.Components.InteractableComponent).raytrace_enter()
 		if target_collider != collider:
 			_collision_with_current_object_finished()
 	else:
 		if rayCastCollisionState:
 			_collision_with_current_object_finished()
+
+func find_target_component_container(inTarget_collider)->component_container:
+	if inTarget_collider is component_container:
+		return inTarget_collider
+	for child in inTarget_collider.get_children():
+		var target_container = find_target_component_container(child)
+		if target_container != null:
+			return target_container
+	return null
 
 func _collision_with_current_object_finished()->void:
 	if target_collider:
