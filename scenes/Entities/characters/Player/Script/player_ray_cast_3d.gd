c4fe480bcbc3c9ccee1aeb48a907b864b4dce620
Roman Marchenko <roma.marchenko@gmail.com>
2026-08-03 16:22:49 +0200

    Fixed component_container search on raycast

--- scenes/Entities/characters/Player/Script/player_ray_cast_3d.gd
+++ scenes/Entities/characters/Player/Script/player_ray_cast_3d.gd
@@ -1,47 +1,64 @@
 extends RayCast3D
 
-const COMPONENT_CONTAINER_TREE_NAME: String = "/ComponentContainer"
+var current_collider: Node = null
+var current_container: component_container = null
 
-##Tracks if raycast hit anything. Required to track when raycast stops heating current object
-var rayCastCollisionState: bool = false
+func _physics_process(_delta: float) -> void:
+	var new_collider = get_collider() if is_colliding() else null
+	
+	# Only update states if the object we are looking at has changed
+	if new_collider != current_collider:
+		_change_target(new_collider)
 
-var target_collider
-var target_component_container
-
-func _input(_event) -> void:
-	if is_colliding():
-		var collider = get_collider()
-		if collider == null: return
+func _change_target(new_collider: Node) -> void:
+	# 1. CLEANUP PREVIOUS TARGET: If we had a previous interactable, trigger exit
+	if current_container:
+		var interactable = current_container.get_component(GameEnums.Components.InteractableComponent)
+		if interactable:
+			interactable.raytrace_exit()
+	# 2. UPDATE TRACKERS
+	current_collider = new_collider
+	current_container = null
+	
+	# 3. INITIALIZE NEW TARGET: If we are looking at a new object, trigger enter
+	if current_collider:
+		var interactable_root = _get_interactable_root(current_collider)
 		
-		if collider.get_parent().is_in_group("iInteractable") && !rayCastCollisionState:
-			target_collider = collider
-			#Var to track so function call send only once
-			rayCastCollisionState = true
-			target_component_container = find_target_component_container(collider.get_parent())
-			if target_component_container:
-				target_component_container.get_component(GameEnums.Components.InteractableComponent).raytrace_enter()
-		if target_collider != collider:
-			_collision_with_current_object_finished()
-	else:
-		if rayCastCollisionState:
-			_collision_with_current_object_finished()
+		# Ensure it exists and is interactable before searching the tree
+		if interactable_root:
+			current_container = _find_component_container(interactable_root)
+			
+			if current_container:
+				var interactable = current_container.get_component(GameEnums.Components.InteractableComponent)
+				if interactable:
+					interactable.raytrace_enter()
 
-func find_target_component_container(inTarget_collider)->component_container:
-	if inTarget_collider is component_container:
-		return inTarget_collider
-	for child in inTarget_collider.get_children():
-		var target_container = find_target_component_container(child)
-		if target_container != null:
-			return target_container
+func _get_interactable_root(node: Node) -> Node:
+	var current_node = node
+	while current_node != null:
+		if current_node.is_in_group("iInteractable"):
+			return current_node
+		current_node = current_node.get_parent()
 	return null
 
-func _collision_with_current_object_finished()->void:
-	if target_collider:
-		target_component_container.get_component(GameEnums.Components.InteractableComponent).raytrace_exit()
-	target_collider = null
-	target_component_container = null
-	rayCastCollisionState = false
+## Recursively searches the given node and all its children for a component_container
+func _find_component_container(node: Node) -> component_container:
+	# Base case: we found it
+	if node is component_container:
+		return node
+		
+	# Recursive case: search through all children
+	for child in node.get_children():
+		var found_container = _find_component_container(child)
+		if found_container != null:
+			return found_container
+			
+	# If not found in this branch
+	return null
 
-func on_interact_input()->void:
-	if target_collider:
-		target_component_container.get_component(GameEnums.Components.InteractableComponent).try_interact()
+## Called externally (e.g., from an Input map action in your Player script)
+func on_interact_input() -> void:
+	if current_container:
+		var interactable = current_container.get_component(GameEnums.Components.InteractableComponent)
+		if interactable:
+			interactable.try_interact()
